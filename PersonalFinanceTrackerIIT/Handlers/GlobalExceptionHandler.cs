5b5507f89fda97ad4e08880a4a7f8460ead3e146
Tayabur Rahman Masud <tayabur.masud@gmail.com>
2025-07-09 13:35:05 +0600

    excception handler added

---
+++ PersonalFinanceTrackerIIT/Handlers/GlobalExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace PersonalFinanceTrackerIIT.Handlers;
+
+public static class GlobalExceptionHandler
+{
+    public static void ThreadException(object sender, ThreadExceptionEventArgs e)
+    {
+        HandleException(e.Exception);
+    }
+
+    public static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        Exception? ex = e.ExceptionObject as Exception;
+        if (ex != null)
+        {
+            HandleException(ex);
+        }
+    }
+
+    private static void HandleException(Exception ex)
+    {
+        LogException(ex);
+        MessageBox.Show("An unexpected error occurred:\n" + ex.Message,
+            "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    }
+
+    private static void LogException(Exception ex)
+    {
+        string logFile = "error_log.txt";
+        File.AppendAllText(logFile,
+            $"[{DateTime.Now}] {ex.Message}\n{ex.StackTrace}\n\n");
+    }
+}
--- PersonalFinanceTrackerIIT/Program.cs
+++ PersonalFinanceTrackerIIT/Program.cs
@@ -1,3 +1,4 @@
+using PersonalFinanceTrackerIIT.Handlers;
 using PersonalFinanceTrackerIIT.UI;
 
 namespace PersonalFinanceTrackerIIT
@@ -12,6 +13,13 @@ namespace PersonalFinanceTrackerIIT
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            Application.ThreadException += new ThreadExceptionEventHandler(GlobalExceptionHandler.ThreadException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(GlobalExceptionHandler.UnhandledException);
+
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             ApplicationConfiguration.Initialize();
             Application.Run(new MainUi());
         }
